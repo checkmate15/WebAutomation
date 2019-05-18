e6c7afdfcfae8f1691e39d6493baac71ae587ec1
opuahmed09 <opuahmed000@gmail.com>
2019-05-18 17:13:25 -0400

    module created

---
+++ Cnn/src/main/java/hompage/Hompage.java
@@ -0,0 +1,9 @@
+package hompage;
+
+import base.CommonAPI;
+
+public class Hompage extends CommonAPI {
+
+
+
+}
---
+++ Cnn/src/test/java/testhompage/TestHomPage.java
@@ -0,0 +1,15 @@
+package testhompage;
+
+import base.CommonAPI;
+import hompage.Hompage;
+
+public class TestHomPage extends CommonAPI {
+    Hompage hpage;
+    String url="https://www.cnn.com/";
+
+
+
+
+}
+
+
