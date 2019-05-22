c58663ae4c6d014bfd979b66e514a00412f96f19
ahnafid <arahmanamin123@gmail.com>
2019-05-22 05:47:10 -0400

    40testcasesupdated

--- Amazon/src/test/java/testtodaysdeal/Testtodaysdeal.java
+++ Amazon/src/test/java/testtodaysdeal/Testtodaysdeal.java
@@ -37,6 +37,36 @@ public class Testtodaysdeal extends CommonAPI {
     public void clickonclear(){
         testamazon2.setClear();
     }
+    @Test(priority = 34)
+    public void clickoncamera(){
+        testamazon2.setCamera();
+    }
+    @Test(priority = 35)
+    public void clickondiscount(){
+        testamazon2.setDiscount();
+    }
+    @Test(priority = 36)
+    public void clickondiscountitem(){
+        testamazon2.setDiscountitem();
+    }
+    @Test(priority = 37)
+    public void clickonaddtolist(){
+        testamazon2.setAddtolist();
+    }
+    @Test(priority = 38)
+    public void clickonforgetpass(){
+        testamazon2.setForgetpass();
+    }
+    @Test(priority = 39)
+    public void clickonenterphone(){
+        testamazon2.setEnterphone();
+    }
+    @Test(priority = 40)
+    public void clickonenter(){
+        testamazon2.setEnter();
+    }
+
+
 
 
 }
